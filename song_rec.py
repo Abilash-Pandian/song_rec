7da4130750e4acf6277f4ff5778dffbc805ca4a5
Abilash-Pandian <102868504+Abilash-Pandian@users.noreply.github.com>
2023-08-09 18:11:52 +0530

    Update song_rec.py

--- song_rec.py
+++ song_rec.py
@@ -1,37 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-
-# Read the CSV file
 df = pd.read_csv("data_moods.csv")
-
-# Drop unnecessary columns
 df = df.drop(columns=['name', 'album', 'artist', 'release_date'])
 
-# Streamlit App
 def main():
     st.title("Mood-Based Song Selector")
-    
-    # Sidebar for user input
-    x = st.sidebar.selectbox("Select Mood:", ["Energetic", "Sad", "Happy", "Calm"])
-    
-    # Filter the DataFrame based on the selected mood
+    x=st.sidebar.selectbox("Select Mood:",["Energetic","Sad","Happy","Calm"])
     filtered_df = df[df['mood'] == x]
-    
-    # Sample 5 songs from the filtered DataFrame
     sampled_songs = filtered_df.sample(n=5)
-    
-    # Display the sampled songs
     st.write("Selected Mood:", x)
     st.write("Spotify id for 5",x,"songs")
     st.dataframe(sampled_songs[['id']])
-    
     for song_id in sampled_songs['id']:
         st.write(f"{song_id}  ")
         copy_button = st.button(f"Copy {song_id}", key=song_id)
         if copy_button:
             pyperclip.copy(song_id)
             st.success(f"{song_id} copied to clipboard!")
-
 if __name__ == "__main__":
     main()
